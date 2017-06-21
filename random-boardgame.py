08ed78667e34647cf065257ceeac7ac499a8cdeb
mcb2003 <mikeybuchan@hotmail.co.uk>
2017-06-21 06:49:54 +0100

    added and tested a roll_dice function

--- random-boardgame.py
+++ random-boardgame.py
@@ -16,10 +16,24 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    email:  mikeybuchan@hotmail.co.uk
 
+#import the random module for rolling the dice
+import random
+
 #create the main board class
 class game(object):
     def __init__(self):
         self.board = "43   44   45   46   47   48   49\n\n42   41   40   39   38   37   36\n\n29   30   31   32   33   34   35\n\n28   27   26   25   24   23   22\n\n15   16   17   18   19   20   21\n\n14   13   12   11   10   09   08\n\n01   02   03   04   05   06   07"
+        self.playerdice = []
+        self.playerdice.append([0,0])
+        self.playerdice.append([0,0])
+    
+    def roll_dice(self,player):
+        for i in range(2):
+            dice = random.randint(1,6)
+            self.playerdice[player-1][0] = dice
+        print("Player ", player, " got a ", self.playerdice[player-1][0], " and a ", self.playerdice[player-1][1], "!")
+        input("Press enter to continue: ")
 
 boardgame = game()
 print(boardgame.board)
+boardgame.roll_dice(1)
